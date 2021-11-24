22dbc77a4e8fd77f0c50b10ed259686a371b9ea4
soulmachine <soulmachine@gmail.com>
2021-11-24 11:39:05 +0000

    cleanup

--- build.rs
+++ build.rs
@@ -1,13 +1,12 @@
 extern crate cbindgen;
 
 use cbindgen::Config;
-use std::env;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let config = Config::from_file("cbindgen.toml").unwrap();
-
-    cbindgen::generate_with_config(&crate_dir, config)
-        .unwrap()
-        .write_to_file("include/crypto_crawler.h");
+    let bindings = {
+        let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let config = Config::from_file("cbindgen.toml").unwrap();
+        cbindgen::generate_with_config(&crate_dir, config).unwrap()
+    };
+    bindings.write_to_file("include/crypto_crawler.h");
 }
